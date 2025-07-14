a4101d29f5074b6cfa14fd160a0030194fc194d4
Roger Huba <rogerhuba@gmail.com>
2025-07-14 16:44:14 -0500

    fix hover on server status

--- src/components/ServerStatus.tsx
+++ src/components/ServerStatus.tsx
@@ -155,9 +155,9 @@ export function ServerStatus({ className = "", showRefresh = false }: ServerStat
         </div>
         
         {/* Tooltip for last updated time */}
-        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-all duration-300 pointer-events-none whitespace-nowrap z-50 scale-95 group-hover:scale-100">
+        <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-all duration-300 pointer-events-none whitespace-nowrap z-50 scale-95 group-hover:scale-100">
           Last updated: {isMounted ? lastUpdated.toLocaleTimeString() : 'Loading...'}
-          <div className="absolute top-full left-1/2 transform -translate-x-1/2 border-4 border-transparent border-t-gray-800"></div>
+          <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 border-4 border-transparent border-b-gray-800"></div>
         </div>
       </div>
     </div>
